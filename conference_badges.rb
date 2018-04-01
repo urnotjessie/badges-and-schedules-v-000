7d4eb81fe0bcd931b5ddc02a1d4051e1676c91ab
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 02:53:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    result = []
+    attendees.each do |attendee|
+        result << badge_maker(attendee)
+    end
+    return result
+end
+
+def assign_rooms(attendees)
+    result = []
+    attendees.each_with_index do |attendee, index|
+        result[index] = "Hello, #{attendee}! You'll be assigned to room #{index}!"
+    end
+    return result
+end
+    
